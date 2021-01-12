f5672f27f0b9bbfab5c14f810b0d0e3633e46414
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-12 15:20:51 +0700

    Commit

---
+++ app/src/main/java/com/example/fragmentexample1/SimpleFragment.java
@@ -0,0 +1,29 @@
+package com.example.fragmentexample1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+
+public class SimpleFragment extends Fragment {
+
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView =
+                inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+        return rootView;
+    }
+}
